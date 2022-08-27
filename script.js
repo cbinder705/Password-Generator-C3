3dc36f9a7234c382fd18d891798d0d335b3a44e1
Cory Bee <cbinder705@gmail.com>
2022-08-27 00:01:18 -0400

    progress

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-window.prompt("")
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+ 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+let question1 = prompt('Select character amount between 8 & 128');
+let question2 = prompt('Include lowercase?');
+let question3 = prompt('Include uppercase?');
+let question4 = prompt('Include numbers?');
+let question5 = prompt('Include special characters?');
+
+
+
+console.log(question1);
+console.log(question2);
+console.log(question3);
+console.log(question4);
+console.log(question5);
