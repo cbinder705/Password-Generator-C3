f8a3fbb24c07626532e55d18cd451c7c14ae3ef9
Cory Bee <cbinder705@gmail.com>
2022-08-29 18:05:36 -0400

    added if else for choices

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-var numbers="01234567890"
+// variables for custom password options
+var numbers="0123456789"
 var lowercase="abcedefghijklmnopqrstuvwxyz"
 var upercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var special = "!@#$%^&*"
+var special = "!@#$%^&*()></"
+let pw = " ";
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var pw = generatePassword();
   var passwordText = document.querySelector("#password");
  
   passwordText.value = password;
@@ -17,25 +19,35 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePasword(){
-  let pw = " ";
-  let passwordLength= 0;
-  let numbers;
-  let lowercase;
-  let upercase;
-  let special;
+  let passwordlength = prompt('Please select a character amount between 8 & 128.');
+  if(passwordlength <8 , passwordlength > 128);
+  let littleLetters = confirm('Include lowercase?');
+  if (littleLetters){
+    pw+=lowercase
+  };
+  let bigLetters = confirm('Include uppercase?');
+  if (bigLetters) {
+    pw+=upercase
+  };
+  let num = confirm('Include numbers?');
+  if (num){
+    pw+=numbers
+  };
+  let sCharacters = confirm('Include special characters?');
+  if(sCharacters){
+    pw+=special
+  };
+
 }
 
 
-let question1 = prompt('Select character amount between 8 & 128');
-let question2 = prompt('Include lowercase?');
-let question3 = prompt('Include uppercase?');
-let question4 = prompt('Include numbers?');
-let question5 = prompt('Include special characters?');
+
+generatePasword();
 
 
 
-console.log(question1);
-console.log(question2);
-console.log(question3);
-console.log(question4);
-console.log(question5);
+console.log(passwordlength);
+console.log(littleLetters);
+console.log(bigLetters);
+console.log(num);
+console.log(sCharacters);
