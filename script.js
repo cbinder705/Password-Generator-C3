54f7bb97b62124ba4b2f7ff60b79a1140647271b
Cory Bee <cbinder705@gmail.com>
2022-08-29 22:24:09 -0400

    button still not working.

--- script.js
+++ script.js
@@ -3,8 +3,48 @@ var numbers="0123456789"
 var lowercase="abcedefghijklmnopqrstuvwxyz"
 var upercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var special = "!@#$%^&*()></"
-let pw = " ";
+var pw = " ";
 var generateBtn = document.querySelector("#generate");
+
+function generatePasword(){
+  let passwordlength = prompt('Please select a character amount between 8 & 128.');
+  if(passwordlength <8 || passwordlength > 128 || isNaN(parseInt(passwordlength)));{
+    alert('Please enter a number between 8 and 128.');
+  }else 
+    {
+  let littleLetters = confirm('Include lowercase?');
+  if (littleLetters){
+    pw+=lowercase;
+  }
+  let bigLetters = confirm('Include uppercase?');
+  if (bigLetters) {
+    pw+=upercase;
+  }
+  let num = confirm('Include numbers?');
+  if (num){
+    pw+=numbers;
+  }
+  let sCharacters = confirm('Include special characters?');
+  if(sCharacters){
+    pw+=special;
+  }
+}
+let pword= " "
+for (let i=0; i<passwordlength; i++)
+pword += pw.charAt(Math.floor(Math.random() * pw.length))
+return pword;
+}
+
+function writePassword()
+
+
+
+console.log(passwordlength);
+console.log(littleLetters);
+console.log(bigLetters);
+console.log(num);
+console.log(sCharacters);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -18,36 +58,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-function generatePasword(){
-  let passwordlength = prompt('Please select a character amount between 8 & 128.');
-  if(passwordlength <8 , passwordlength > 128);
-  let littleLetters = confirm('Include lowercase?');
-  if (littleLetters){
-    pw+=lowercase
-  };
-  let bigLetters = confirm('Include uppercase?');
-  if (bigLetters) {
-    pw+=upercase
-  };
-  let num = confirm('Include numbers?');
-  if (num){
-    pw+=numbers
-  };
-  let sCharacters = confirm('Include special characters?');
-  if(sCharacters){
-    pw+=special
-  };
-
-}
-
-
-
-generatePasword();
-
-
-
-console.log(passwordlength);
-console.log(littleLetters);
-console.log(bigLetters);
-console.log(num);
-console.log(sCharacters);
